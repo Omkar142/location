397cf871a06fd5255e5e4e466f6cfdc842a6c0cb
Omkar142 <54744919+Omkar142@users.noreply.github.com>
2021-12-10 13:14:21 +0530

    Update script.js

--- script.js
+++ script.js
@@ -21,8 +21,8 @@
         .then(response => response.json()).then(response =>{
             let allDetails = response.results[0].components; //passing components object to allDetails variable
             console.table(allDetails);
-            let {county, postcode, country} = allDetails; //getting country, postcode, country properties value from allDetails obj
-            button.innerText = `${county} ${postcode}, ${country}`; //passing these value to the button innerText
+           let {county, postcode, road, suburb, neighbourhood, state, state_district, country, continent} = allDetails; //getting country, postcode, country properties value from allDetails obj
+            button.innerText = `${county}, ${postcode}, ${road}, ${suburb}, ${neighbourhood}, ${state},  ${state_district},${country}, ${continent}`; //passing these value to the button innerText
         }).catch(()=>{ //if error any error occured
             button.innerText = "Something went wrong";
         });
