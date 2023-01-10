a68e2aaa80edea33ddaa28a677b9c964ee9ebf74
Omkar142 <54744919+Omkar142@users.noreply.github.com>
2023-01-10 13:05:04 +0530

    Delete dictionary/DigitalDictionary/src/main directory

--- dictionary/DigitalDictionary/src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.minidictionary {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.minidictionary to javafx.fxml;
-    exports com.example.minidictionary;
-}
